91e5a83d463cdc6fe71bca5ed85f0f49405e05b9
Aleksandr Gurin <sanyagurin747@gmail.com>
2023-09-28 18:02:10 +0300

    JRTB-2: Init project

--- javarush-telegrambot/src/main/java/com/github/javarushcommunity/jrtb/JavarushTelegramBotApplication.java
+++ javarush-telegrambot/src/main/java/com/github/javarushcommunity/jrtb/JavarushTelegramBotApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JavarushTelegramBotApplication {
 
 	public static void main(String[] args) {
+		System.out.println("piska");
 		SpringApplication.run(JavarushTelegramBotApplication.class, args);
 	}
 
