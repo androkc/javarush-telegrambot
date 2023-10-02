7f435578e6b01d138c30277946dbd8958af5e2e7
Petr <125360919+androkc@users.noreply.github.com>
2023-10-02 15:27:14 +0300

    JRTB-3: Implemented Command pattern for handling Telegram Bot commands (#17)
    * JRTB-3: little commit
    
    * JRTB-3: added Command pattern for handling Telegram Bot commands
    
    * JRTB-3: updated RELEASE_NOTES.md
    
    * JRTB-3: updated RELEASE_NOTES.md

---
+++ javarush-telegrambot/src/main/java/com/github/javarushcommunity/jrtb/command/StartCommand.java
@@ -0,0 +1,18 @@
+package com.github.javarushcommunity.jrtb.command;
+
+import com.github.javarushcommunity.jrtb.service.SendBotMessageService;
+import lombok.AllArgsConstructor;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+@AllArgsConstructor
+public class StartCommand implements Command {
+    private final SendBotMessageService sendBotMessageService;
+    public final static String START_MESSAGE = "Привет. Я Javarush Telegram Bot. Я помогу тебе быть в курсе последних " +
+                                                    "статей тех авторов, котрые тебе интересны. Я еще маленький и только учусь.";
+
+
+    @Override
+    public void execute(Update update) {
+        sendBotMessageService.sendMessage(update.getMessage().getChatId().toString(), START_MESSAGE);
+    }
+}
---
+++ javarush-telegrambot/src/test/java/com/github/javarushcommunity/jrtb/command/StartCommandTest.java
@@ -0,0 +1,21 @@
+package com.github.javarushcommunity.jrtb.command;
+
+import static com.github.javarushcommunity.jrtb.command.CommandNameEnum.START;
+import static com.github.javarushcommunity.jrtb.command.StartCommand.START_MESSAGE;
+
+public class StartCommandTest extends AbstractCommandTest {
+    @Override
+    String getCommandName() {
+        return START.getCommandName();
+    }
+
+    @Override
+    String getCommandMessage() {
+        return START_MESSAGE;
+    }
+
+    @Override
+    Command getCommand() {
+        return new StartCommand(sendBotMessageService);
+    }
+}
